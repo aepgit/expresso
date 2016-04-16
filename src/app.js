0987d668e1ba0f4c80bf7fd5c67c3536b7aaa5e8
Antti <aepohjola@gmail.com>
2016-04-16 16:07:17 +0300

    personalization

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express'),
+	request = require('request'),
 	  posts = require('./mock/posts.json');
 
 var postsLists = Object.keys(posts).map(function(value) {
@@ -13,16 +14,46 @@ app.use('/static', express.static(__dirname + '/public'))
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
+var dotapost = [];
+request('https://www.reddit.com/r/dota2.json', function(err, resp, body) {
+ body = JSON.parse(body);
+ 
+ var resarray = []; 
+ for(let i = 3; i < 25; i++)
+ {
+	 var childdata = body.data.children[i].data;
+	 if(childdata.selftext != "")
+	 {
+		var childobj = {};
+		childobj.title = childdata.title;
+		childobj.description = childdata.selftext;
+		resarray.push(childobj);
+	 }
+ }
+ dotapost = resarray;
+});
+
+
 app.get('/', function(req, res){
+	var path = req.path;
+	res.locals.path = path;
+	// COULD USE
+	// res.render('index', {path:path});
 	res.render('index');
 });
+
+
 
 app.get('/blog/:title?', function(req, res){ 
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.render('blog', {posts: postsLists})
-	} else {
+		//res.render('blog', {posts: postsLists})
+		res.render('blog', {posts: dotapost});
+	} else if (title == 'dota'){		
+		res.status(503);
+		res.render('blog', {posts: dotapost});
+	}else {
 		var post = posts[title] || {};
 		res.render('post', { post: post});
 	}
