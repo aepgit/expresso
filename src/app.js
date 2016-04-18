3cb788317e01637195865b31e8f1c9360c6b491a
Antti <aepohjola@gmail.com>
2016-04-18 22:06:59 +0300

    details

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 
 var express = require('express'),
 	request = require('request'),
-	  posts = require('./mock/posts.json');
+	posts = require('./mock/posts.json');
 
 var postsLists = Object.keys(posts).map(function(value) {
-							         return posts[value]})
+	return posts[value]
+})
 
 var app = express();
 
@@ -15,26 +16,26 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
 var dotapost = [];
-request('https://www.reddit.com/r/dota2.json', function(err, resp, body) {
- body = JSON.parse(body);
- 
- var resarray = []; 
- for(let i = 3; i < 25; i++)
- {
-	 var childdata = body.data.children[i].data;
-	 if(childdata.selftext != "")
-	 {
+request('https://www.reddit.com/r/programming.json', function(err, resp, body) {
+	body = JSON.parse(body);
+
+	var resarray = [];
+	for (let i = 3; i < 25; i++) {
+		var childdata = body.data.children[i].data;
+		//if(childdata.selftext != "")
+		//{
 		var childobj = {};
 		childobj.title = childdata.title;
-		childobj.description = childdata.selftext;
+		childobj.description = childdata.domain;
+		childobj.url = childdata.url;
 		resarray.push(childobj);
-	 }
- }
- dotapost = resarray;
+		//}
+	}
+	dotapost = resarray;
 });
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	var path = req.path;
 	res.locals.path = path;
 	// COULD USE
@@ -44,18 +45,24 @@ app.get('/', function(req, res){
 
 
 
-app.get('/blog/:title?', function(req, res){ 
+app.get('/blog/:title?', function(req, res) {
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
 		//res.render('blog', {posts: postsLists})
-		res.render('blog', {posts: dotapost});
-	} else if (title == 'dota'){		
+		res.render('blog', {
+			posts: dotapost
+		});
+	} else if (title == 'dota') {
 		res.status(503);
-		res.render('blog', {posts: dotapost});
-	}else {
+		res.render('blog', {
+			posts: dotapost
+		});
+	} else {
 		var post = posts[title] || {};
-		res.render('post', { post: post});
+		res.render('post', {
+			post: post
+		});
 	}
 });
 
